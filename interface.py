089fee097f08fa0166ece2ca0e51983e96ee8828
José Mariano <jose.mariano2002@outlook.com>
2020-07-15 20:52:43 -0300

    o básico da interface foi criado

---
+++ interface.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+class Application:
+	def __init__(self, master=None):
+		pass
+
+
+
+
+window = tk.Tk()
+window.title('Criando conta')
+Application(window)
+window.mainloop()
