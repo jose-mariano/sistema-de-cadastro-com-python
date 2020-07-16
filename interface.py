dbb6d64a853c0713a79952829ea700c58c64bacc
José Mariano <jose.mariano2002@outlook.com>
2020-07-15 21:44:16 -0300

    Interface inicial pronta

--- interface.py
+++ interface.py
@@ -2,31 +2,36 @@ import tkinter as tk
 
 class Application:
 	def __init__(self, master=None):
-		self.containerMain = tk.Frame(master)
+		self.backgroundColor = 'white'
+
+		self.containerMain = tk.Frame(master, bg=self.backgroundColor)
 		self.containerMain.pack()
 
-		self.containerUsername = tk.Frame(self.containerMain, padx=20, pady=10)
+		self.containerUsername = tk.Frame(self.containerMain, padx=20, pady=10, bg=self.backgroundColor)
 		self.containerUsername.pack()
-		self.messageUsername = tk.Label(self.containerUsername, text='Username', padx=10, pady=10)
+		self.messageUsername = tk.Label(self.containerUsername, text='Username', padx=10, pady=10, bg=self.backgroundColor)
 		self.messageUsername.pack(anchor='w')
-		self.getUsername = tk.Entry(self.containerUsername, width=30)
+		self.getUsername = tk.Entry(self.containerUsername, width=30, bg=self.backgroundColor)
+		self.getUsername.config(highlightbackground='black')
 		self.getUsername.pack()
 
-		self.containerPassword = tk.Frame(self.containerMain, padx=20, pady=10)
+		self.containerPassword = tk.Frame(self.containerMain, padx=20, pady=10, bg=self.backgroundColor)
 		self.containerPassword.pack()
-		self.messagePassword = tk.Label(self.containerPassword, text='Password', padx=10, pady=10)
+		self.messagePassword = tk.Label(self.containerPassword, text='Password', padx=10, pady=10, bg=self.backgroundColor)
 		self.messagePassword.pack(anchor='w')
-		self.getPassword = tk.Entry(self.containerPassword, width=30)
+		self.getPassword = tk.Entry(self.containerPassword, width=30, bg=self.backgroundColor)
+		self.getPassword.config(highlightbackground='black')
 		self.getPassword.pack()
 
-		self.containerConfirmPassword = tk.Frame(self.containerMain, padx=20, pady=10)
+		self.containerConfirmPassword = tk.Frame(self.containerMain, padx=20, pady=10, bg=self.backgroundColor)
 		self.containerConfirmPassword.pack()
-		self.messageConfirmPassword = tk.Label(self.containerConfirmPassword, text='Confirm Password', padx=10, pady=10)
+		self.messageConfirmPassword = tk.Label(self.containerConfirmPassword, text='Confirm Password', padx=10, pady=10, bg=self.backgroundColor)
 		self.messageConfirmPassword.pack(anchor='w')
-		self.getConfirmPassword = tk.Entry(self.containerConfirmPassword, width=30)
+		self.getConfirmPassword = tk.Entry(self.containerConfirmPassword, width=30, bg=self.backgroundColor)
+		self.getConfirmPassword.config(highlightbackground='black')
 		self.getConfirmPassword.pack()
 
-		self.containerButton = tk.Frame(self.containerMain, padx=20, pady=20)
+		self.containerButton = tk.Frame(self.containerMain, padx=20, pady=20, bg=self.backgroundColor)
 		self.containerButton.pack()
 		self.buttonCreateAccount = tk.Button(self.containerButton, text='Create Account')
 		self.buttonCreateAccount.pack()
@@ -35,7 +40,6 @@ class Application:
 
 window = tk.Tk()
 window['pady'] = 40
-window['bg'] = 'white'
 window.title('Creating a new account')
 window.geometry('400x400')
 Application(window)
