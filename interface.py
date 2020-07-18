2e380e21abab6ea5530058db1aec9dc9174a97be
José Mariano <jose.mariano2002@outlook.com>
2020-07-17 21:08:46 -0300

    Métodos createAccount() e getEntryValues() adicionados em interface.py

--- interface.py
+++ interface.py
@@ -20,6 +20,7 @@ class Application:
 		self.messagePassword = tk.Label(self.containerPassword, text='Password', padx=10, pady=10, bg=self.backgroundColor)
 		self.messagePassword.pack(anchor='w')
 		self.getPassword = tk.Entry(self.containerPassword, width=30, bg=self.backgroundColor)
+		self.getPassword['show'] = '*'
 		self.getPassword.config(highlightbackground='black')
 		self.getPassword.pack()
 
@@ -28,13 +29,25 @@ class Application:
 		self.messageConfirmPassword = tk.Label(self.containerConfirmPassword, text='Confirm Password', padx=10, pady=10, bg=self.backgroundColor)
 		self.messageConfirmPassword.pack(anchor='w')
 		self.getConfirmPassword = tk.Entry(self.containerConfirmPassword, width=30, bg=self.backgroundColor)
+		self.getConfirmPassword['show'] = '*'
 		self.getConfirmPassword.config(highlightbackground='black')
 		self.getConfirmPassword.pack()
 
 		self.containerButton = tk.Frame(self.containerMain, padx=20, pady=20, bg=self.backgroundColor)
 		self.containerButton.pack()
 		self.buttonCreateAccount = tk.Button(self.containerButton, text='Create Account')
+		self.buttonCreateAccount['command'] = self.createAccount
 		self.buttonCreateAccount.pack()
+	
+
+	def createAccount(self):
+		self.getEntryValues()
+
+
+	def getEntryValues(self):
+		self.username = str(self.getUsername.get())
+		self.password = str(self.getPassword.get())
+		self.confirmPassword = str(self.getConfirmPassword.get())
 
 
 
