66d7f0d05fcc0c0a54b18c98aabdcafe8fcf6aa6
José Mariano <jose.mariano2002@outlook.com>
2020-09-20 14:09:03 -0300

    Configuração da página e alteração do README.md

--- system.py
+++ system.py
@@ -6,4 +6,7 @@ database = db.Database('register.db')
 pages = (frames.StartPage, frames.SeeRegisteredPeople, frames.RegistrationPage)
 
 system = frames.FrameManipulation(pages=pages, database=database)
+system.title('Sistema de Cadastro')
+system.geometry('600x400+100+50')
+system.resizable(False, False)
 system.mainloop()
